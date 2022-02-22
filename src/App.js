83f3360e2a226e0a245f43a16b19de5ce931f32f
lisandroseia <lisandroseia11@gmail.com>
2022-02-22 19:15:21 -0300

    set global store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import NavBar from './components/navbar';
-import Books from './redux/books/books';
-import Categories from './redux/categories/categories';
+import Books from './components/BooksSection';
+import Categories from './components/CategoriesSection';
 import './App.css';
+import store from './redux/configStore';
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="categories" element={<Categories />} />
-        </Routes>
+        <Provider store={store}>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="categories" element={<Categories />} />
+          </Routes>
+        </Provider>
       </Router>
     </>
   );
